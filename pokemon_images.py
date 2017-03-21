958f6ab71df82147ee4c3611afdd81a3ac127007
Erik Fahlén <erfa90@gmail.com>
2017-03-21 20:51:18 +0100

    Figure out URL to artwork.

--- pokemon_images.py
+++ pokemon_images.py
@@ -1,3 +1,9 @@
+import urllib2
+import json
+
+opener = urllib2.build_opener()
+opener.addheaders = [('User-Agent', 'Pokemon')]
+
 def parse_pokemon(filename):
 	with open(filename) as f:
 		lines = f.readlines()
@@ -5,6 +11,13 @@ def parse_pokemon(filename):
 	content = u','.join(lines)
 	return [pokemon.strip() for pokemon in content.split(',')]
 
+def get_pokemon_image_url(pokemon):
+	url = u'http://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon.lower())
+	response = opener.open(url)
+	data = json.load(response)
+	return u'https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/other-sprites/official-artwork/{}.png'.format(data['id'])
+
 if __name__ == '__main__':
 	pokemon = parse_pokemon('pokemon.txt')
-	print(pokemon)
+	url = get_pokemon_image_url(pokemon[0])
+	print url
