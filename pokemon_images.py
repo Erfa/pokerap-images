9c3d1b2f09bf2cad96a5c3262f5d724652630b91
Erik Fahlén <erfa90@gmail.com>
2017-03-21 21:32:01 +0100

    Do some image processing.

--- pokemon_images.py
+++ pokemon_images.py
@@ -1,8 +1,14 @@
+import urllib
 import urllib2
+import cStringIO
 import json
+from PIL import Image
+
 
 opener = urllib2.build_opener()
 opener.addheaders = [('User-Agent', 'Pokemon')]
+
+bg = Image.open('bg.png')
 
 def parse_pokemon(filename):
 	with open(filename) as f:
@@ -15,9 +21,19 @@ def get_pokemon_image_url(pokemon):
 	url = u'http://pokeapi.co/api/v2/pokemon/{}/'.format(pokemon.lower())
 	response = opener.open(url)
 	data = json.load(response)
-	return u'https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/other-sprites/official-artwork/{}.png'.format(data['id'])
+	return u'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other-sprites/official-artwork/{}.png'.format(data['id'])
+
+def get_pokemon_image(url):
+	img = Image.open(opener.open(url))
+	height = 400
+	width = height * img.width / img.height
+	return img.resize([width, height], Image.BICUBIC)
 
 if __name__ == '__main__':
 	pokemon = parse_pokemon('pokemon.txt')
 	url = get_pokemon_image_url(pokemon[0])
-	print url
+	img = get_pokemon_image(url)
+
+	bg.paste(img, [int(0.5 * bg.width - 0.5 * img.width), int(0.6 * bg.height - 0.5 * img.height)], img)
+
+	bg.save('out.png')
